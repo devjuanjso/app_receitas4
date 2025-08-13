4a4ece3d886cf6be1b73b55a9d76a864669133f2
juanoliveira <juanoliveira@mpam.mp.br>
2025-08-13 09:42:05 -0400

    Detalhes de receitas e tabela de favoritos e perfil

--- lib/data/repositories/recipe_repository.dart
+++ lib/data/repositories/recipe_repository.dart
@@ -1,6 +1,7 @@
 import 'package:app4_receitas/data/models/recipe.dart';
 import 'package:app4_receitas/data/services/recipe_service.dart';
 import 'package:app4_receitas/di/service_locator.dart';
+
 class RecipeRepository {
   final _service = getIt<RecipeService>();
 
@@ -11,6 +12,19 @@ class RecipeRepository {
     } catch (e) {
       throw Exception('Falha ao carregar receitas: ${e.toString()}');
     }
+  }
+
+  Future<Recipe?> getRecipeById(String id) async {
+    final rawData = await _service.fetchRecipeById(id);
+    return rawData != null ? Recipe.fromJson(rawData) : null;
+  }
+
+  Future<List<Recipe>> getFavRecipes(String userId) async {
+    final rawData = await _service.fetchFavRecipes(userId);
+    return rawData
+      .where((data) => data['recipes'] != null)
+      .map((data) => Recipe.fromJson(data['recipes'] as Map<String, dynamic>))
+      .toList();
   }
 
 }
